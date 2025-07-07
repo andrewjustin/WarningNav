fb9007e7b99b8760eae11faa60e9ca3071759748
Andrew Justin <andrewjustin@ou.edu>
2025-07-06 23:24:01 -0500

    added mechanism that bounds frame to tiles*
    * this feature currently only works for 2560x1440 displays

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+"""
+TODO
+    * HIGH priority
+        - prevent scrolling off window where tiles do not exist (currently only works for 2560x1440)
+        - allow selection of polygons that are on top of each other
+        - creating warning list widget
+        - allow toggling of offline tiles
+        - fix warning popup text (some text is missing and level of detail is not consistent between warnings)
+    * MEDIUM priority
+        - customizable warning view options (e.g., only show tornado warnings)
+        - only allow a maximum of one warning popup per polygon
+        - add user option for warning update frequency
+    * LOW priority
+        - create config files for app settings and user-defined markers
+        - figure out realtime clock placement
+"""
+
 import time
 import customtkinter as ctk
 import tkinter as tk
@@ -27,6 +44,8 @@ class AlertDashboard(ctk.CTk):
         self.option_add('*Text.Font', 'Helvetica 14')
         self.option_add('*Menu.Font', 'Helvetica 14')
         
+        self.bind("<B1-Motion>", self._map_motion)
+        
         ######################################### general map widget settings ##########################################
         
         # load offline tiles if available
@@ -41,11 +60,11 @@ class AlertDashboard(ctk.CTk):
         self.map_widget.fit_bounding_box((49.3457868, -124.7844079), (24.7433195, -66.9513812))  # CONUS
         self.map_widget.max_zoom = 12  # prevents zooming in too far where there are no offline tiles
         self.map_widget.min_zoom = 6  # prevents zooming out beyond CONUS coverage
+        self._define_map_bounds()
         
         self.bind('<Configure>', lambda event: self.on_resize(event))  # forces the map to stay centered in the window
         self.map_widget.bind('<Configure>')  # overwrite default map behavior for window resizing
         self.map_widget.pack_propagate(False)
-        
         #################################################### menubar ###################################################
         
         menubar = tk.Menu(self)
@@ -68,6 +87,25 @@ class AlertDashboard(ctk.CTk):
         
         self.mainloop()
     
+    def _define_map_bounds(self):
+        # {zoom: [left, top, right, bottom]}
+        self.map_bounds = {6: [9, 21, 22, 29],
+                           7: [19, 43, 42, 56],
+                           8: [39, 87, 82, 111],
+                           9: [78, 175, 162, 221],
+                           10: [157, 350, 323, 441],
+                           11: [314, 700, 645, 880],
+                           12: [628, 1400, 1288, 1759]}
+        
+        # {zoom: [left (x), top (y), right (x), bottom (y)]
+        self.center_positions = {6: [-101.25, 41.75, -84.371, 30.255],
+                                 7: [-112.504, 45.790, -75.937, 28.666],
+                                 8: [-118.129, 47.377, -71.714, 26.617],
+                                 9: [-121.641, 48.157, -69.608, 25.575],
+                                 10: [-123.047, 48.774, -68.200, 25.052],
+                                 11: [-123.926, 49.079, -67.5, 24.95],
+                                 12: [-124.365, 49.231, -67.235, 24.818]}
+    
     def on_resize(self, event, tolerance_x=50, tolerance_y=50):
         """
         Called whenever the main window is resized or moved.
@@ -86,7 +124,7 @@ class AlertDashboard(ctk.CTk):
            event.height - self.map_widget.winfo_height() > tolerance_y:
             self.map_widget.config(width=event.width, height=event.height)
             self.map_widget.pack()
-    
+        
     def create_debug_log(self, multithreading: bool = False):
         """
         Route all stdout and stderr writes to the debug logger window.
@@ -107,6 +145,37 @@ class AlertDashboard(ctk.CTk):
         else:
             _main()
     
+    def _map_motion(self, event):
+        frame_width = self.map_widget.winfo_width()
+        frame_height = self.map_widget.winfo_height()
+        zoom = int(self.map_widget.last_zoom)
+        
+        # bounds for the tiles (frame must not scroll past these)
+        left_x, top_y, right_x, bottom_y = self.map_bounds[zoom]
+        
+        # tile positions
+        ul_x, ul_y = self.map_widget.upper_left_tile_pos
+        lr_x, lr_y = self.map_widget.lower_right_tile_pos
+        
+        y, x = self.map_widget.get_position()
+        
+        if ul_x < left_x:  # if the widget scrolls left of the riles
+            new_x = self.center_positions[zoom][0]
+            self.map_widget.set_position(y, new_x)
+            sys.stdout.write("Map repositioned due to scrolling left of tiles")
+        elif ul_y < top_y:  # if the widget scrolls above the tiles
+            new_y = self.center_positions[zoom][1]
+            self.map_widget.set_position(new_y, x)
+            sys.stdout.write("Map repositioned due to scrolling above tiles")
+        elif lr_x > right_x:  # if the widget scrolls right of the tiles
+            new_x = self.center_positions[zoom][2]
+            self.map_widget.set_position(y, new_x)
+            sys.stdout.write("Map repositioned due to scrolling right of tiles")
+        elif lr_y > bottom_y:  # if the widget scrolls below the tiles
+            new_y = self.center_positions[zoom][3]
+            self.map_widget.set_position(new_y, x)
+            sys.stdout.write("Map repositioned due to scrolling below tiles")
+
     def _display_active_alerts(self, update_freq: int = 60):
         """
         Complete workflow for updating active alerts.
